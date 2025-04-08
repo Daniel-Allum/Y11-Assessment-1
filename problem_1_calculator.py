b12f4c49875b001669f00cf35fe228db774b3f43
Daniel-Allum <daniel.allum@education.nsw.gov.au>
2025-04-08 03:20:31 +0000

    commit

--- problem_1_calculator.py
+++ problem_1_calculator.py
@@ -1,22 +1,22 @@
 from testhelper import test
 
 def calculate(input1, operator, input2):
-    pass #Replace with your code
 
+    if operator == "+":
+        result = input1 + input2
+    elif operator == "-":
+        result = input1 - input2
+    elif operator == "*":
+        result = input1 * input2
+    elif operator == "/":
+        result = input1 / input2
+    else:
+        result = "Invalid operator"
+    return result
 
+input1 = int(input('Enter a number: '))
+operator = input('Enter a form of operations: ')
+input2 = int(input('Enter a number: '))
 
-
-
-
-
-
-
-### TESTS - Run the code that calls the function to check it works.
-test("Calculator 1", "20", calculate(5, "+", 15))
-test("Calculator 2", "200", calculate(20, "*", 10))
-test("Calculator 3", "2", calculate(100, "/", 50))
-test("Calculator 4", "8", calculate(10, "-", 2))
-test("Calculator 5", "10", calculate(1, "+", 9))
-test("Calculator 6", "20", calculate(2, "*", 10))
-test("Calculator 7", "50", calculate(100, "/", 2))
-test("Calculator 8", "20", calculate(22, "-", 2))
+result = calculate(input1, operator, input2)
+print(f"The result is: {result}")
