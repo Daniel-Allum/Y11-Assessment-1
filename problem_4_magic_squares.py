cb3cc5ba5fa66a0a2d871db535c186d58e75db9e
Daniel-Allum <daniel.allum@education.nsw.gov.au>
2025-04-08 03:59:06 +0000

    commit

--- problem_4_magic_squares.py
+++ problem_4_magic_squares.py
@@ -1,21 +1,31 @@
 from testhelper import test
 
 def is_square_magic(square):
-    pass # Replace this with your code
+    magic_sum = sum(square[0])
+    
+    for i in range(3):
+        if sum(square[i]) != magic_sum or sum(square[j][i] for j in range(3)) != magic_sum:
+            return False
+    
+    if sum(square[i][i] for i in range(3)) != magic_sum or sum(square[i][2-i] for i in range(3)) != magic_sum:
+        return False
+    
+    return True
 
+def create_magic_square():
+    return [[4, 9, 2],
+            [3, 5, 7],
+            [8, 1, 6]]
 
-
-
-
-
-### TESTS - Run the code that calls the function to check it works.
-valid_square = [[4, 9, 2], 
-          [3, 5, 7], 
-          [8, 1, 6]]
-
+valid_square = create_magic_square()
 invalid_square = [[4, 9, 2], 
-          [8, 1, 6],
-          [3, 5, 7]]
+                  [8, 1, 6],
+                  [3, 5, 7]]
 
 test("Magic Square 1", True, is_square_magic(valid_square))
 test("Magic Square 2", False, is_square_magic(invalid_square))
+
+row_sums = [sum(row) for row in valid_square]
+col_sums = [sum(valid_square[i][j] for i in range(3)) for j in range(3)]
+print("Row sums:", row_sums)
+print("Column sums:", col_sums)
