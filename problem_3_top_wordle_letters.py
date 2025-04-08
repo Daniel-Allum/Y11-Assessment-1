ebf6f4d80939dac5fccba0265adb6e376ae2f6e4
Daniel-Allum <daniel.allum@education.nsw.gov.au>
2025-04-08 03:52:07 +0000

    commit

--- problem_3_top_wordle_letters.py
+++ problem_3_top_wordle_letters.py
@@ -13,6 +13,19 @@ def get_valid_wordle_words():
 # all_words will contains a list of all valid words that you can use
 all_words = get_valid_wordle_words()
 
+letter_counts = {}
+for word in all_words:
+    for letter in word:
+        if letter in letter_counts:
+            letter_counts[letter] += 1
+        else:
+            letter_counts[letter] = 1
+
+most_common_letter = max(letter_counts, key=letter_counts.get)  
+count = letter_counts[most_common_letter]
+
+print(f"The most used letter is '{most_common_letter}' and it occurs {count} times.")
+
 
 
 
